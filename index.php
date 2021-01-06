382232a854044d8ab40b8bec32135e6e71b53d73
sanCamelStyle <49991982+sanCamelStyle@users.noreply.github.com>
2021-01-06 16:29:18 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,370 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Yaroflan.lan</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<link rel="stylesheet" href="css/themify-icons.css">
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<!-- Superfish -->
+	<link rel="stylesheet" href="css/superfish.css">
+	<!-- Easy Responsive Tabs -->
+	<link rel="stylesheet" href="css/easy-responsive-tabs.css">
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Theme Style -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="css/preloader.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js" defer></script>
+
+	<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js" defer></script>
+	<script src="https://code.jquery.com/jquery-1.10.2.min.js" defer></script>
+
+</head>
+<body>
+	<!-- START #fh5co-header -->
+	<header id="fh5co-header-section" role="header" class="" >
+		<div class="container">
+
+			<h1 id="fh5co-logo" class="pull-left"><a href="#"><img style="width: 80px;" src="" alt="Yaroflan"></a></h1>
+
+			<!-- START #fh5co-menu-wrap -->
+			<nav id="fh5co-menu-wrap" role="navigation">
+
+
+				<ul class="sf-menu" id="fh5co-primary-menu">
+					<li class="active">
+						<a href="index.php">Главная</a>
+					</li>
+				</ul>
+			</nav>
+			<!-- </div> -->
+
+		</div>
+	</header>
+
+	<div id="fh5co-hero" style="background-image: url(images/img_5.jpg);">
+		<div style="background: #222;" class="overlay"></div>
+		<a href="#fh5co-main" class="smoothscroll fh5co-arrow to-animate hero-animate-4"><i class="ti-angle-down"></i></a>
+		<!-- End fh5co-arrow -->
+		<div class="container">
+			<div class="col-md-12">
+				<div class="fh5co-hero-wrap">
+					<div class="fh5co-hero-intro">
+						<h1 style="font-size: 50px;" class="to-animate hero-animate-1">Ярослав Булава</h1>
+						<h1 class="to-animate hero-animate-1">г.Благовещенск</h1>
+						<h2 class="to-animate hero-animate-1">#ТопСтример#ТопГеймер</h2>
+						
+						<p class="to-animate hero-animate-3"><a href="php/signup.php" class="btn btn-outline btn-md">Регистрация</a></p>
+						<p class="to-animate hero-animate-3"><a href="php/login.php" class="btn btn-outline btn-md">Авторизация</a></p>
+					
+				</div>
+			</div>
+		</div>
+	</div>		
+</div>
+
+
+<div id="page-preloader" class="preloader-2">
+	<div class="loader">
+		<span id="load-perc">0%</span>
+	</div>
+</div>
+
+<div id="fh5co-main">
+
+	<div class="fh5co-cards">
+		<div class="container-fluid">
+			<div class="row animate-box">
+				<div class="col-md-12 heading text-center"><h2>Коротко о моей команде...</h2></div>
+			</div>
+			<div class="row">
+				<div class="col-lg-3 col-md-6 col-sm-6 animate-box">
+					<a class="fh5co-card" href="#">
+						<img src="images/ant-tenacity-colored.png" alt="" class="img-responsive">
+						<div class="fh5co-card-body">
+							<h3><!--F=ma--></h3>
+							<p><!--Ярослав он же Yaroflan, окончил шарагу № 26 с коричнево - говнянным аттестатом. Но это не помешало ему стать успешным в будущем!--></p>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6 animate-box">
+					<a class="fh5co-card" href="#">
+						<img src="images/ant-tenacity-colored.png" alt="" class="img-responsive">
+						<div class="fh5co-card-body">
+							<h3><!--Бесконечность - не предел!--></h3>
+							<p><!--Обладает аккаунтом на Twitch - е, на котором, чуть больше 5 000 000 подписчиков! Слова самого Ярослава:"Я не планирую завершать на достигнутом. В моих планах стоит побить число подписчиков PewDiePie - ая!"--></p>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6 animate-box">
+					<a class="fh5co-card" href="#">
+						<img src="images/ant-tenacity-colored.png" alt="" class="img-responsive">
+						<div class="fh5co-card-body">
+							<h3><!--Верь в себя!--></h3>
+							<p><!--С раннего детсва Yaroflan мечтал именно о таком будущем. И вот оно свершилось. Это еще раз доказывает тот факт, что мечты материальны.(Отец Ярослава - Алексей Миллер - директор "Газпрома")--></p>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6 animate-box">
+					<a class="fh5co-card" href="#">
+						<img src="images/ant-tenacity-colored.png" alt="" class="img-responsive">
+						<div class="fh5co-card-body">
+							<h3></h3>
+							<p><!--Ярослав не скрывает своей благодарности учителям, которые  помогли ему  понять, что оценки - это не главное! Главное то, что у тебя в голове!(заслуженный мастер спорта по математике: Ковтун Людмила Григорьевна, а также олимпийский чемпион микросхемных наук Темников Сергей Николаевич).--></p>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="fh5co-cards">
+		<div class="container-fluid">
+			<div class="row animate-box">
+				<div class="col-md-12 heading text-center"><h2>Коротко о команде противника...</h2></div>
+			</div>
+			<div class="row">
+				<div class="col-lg-3 col-md-6 col-sm-6 animate-box">
+					<a class="fh5co-card" href="#">
+						<img src="images/eagle-courage-colored.png" alt="" class="img-responsive">
+						<div class="fh5co-card-body">
+							<h3><!--F=ma--></h3>
+							<p><!--Ярослав он же Yaroflan, окончил шарагу № 26 с коричнево - говнянным аттестатом. Но это не помешало ему стать успешным в будущем!--></p>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6 animate-box">
+					<a class="fh5co-card" href="#">
+						<img src="images/cheetah-fleetness-colored.png" alt="" class="img-responsive">
+						<div class="fh5co-card-body">
+							<h3><!--Бесконечность - не предел!--></h3>
+							<p><!--Обладает аккаунтом на Twitch - е, на котором, чуть больше 5 000 000 подписчиков! Слова самого Ярослава:"Я не планирую завершать на достигнутом. В моих планах стоит побить число подписчиков PewDiePie - ая!"--></p>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6 animate-box">
+					<a class="fh5co-card" href="#">
+						<img src="images/owl-wisdom-colored.png" alt="" class="img-responsive">
+						<div class="fh5co-card-body">
+							<h3><!--Верь в себя!--></h3>
+							<p><!--С раннего детсва Yaroflan мечтал именно о таком будущем. И вот оно свершилось. Это еще раз доказывает тот факт, что мечты материальны.(Отец Ярослава - Алексей Миллер - директор "Газпрома")--></p>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6 animate-box">
+					<a class="fh5co-card" href="#">
+						<img src="images/shunk-potency-colored.png" alt="" class="img-responsive">
+						<div class="fh5co-card-body">
+							<h3></h3>
+							<p><!--Ярослав не скрывает своей благодарности учителям, которые  помогли ему  понять, что оценки - это не главное! Главное то, что у тебя в голове!(заслуженный мастер спорта по математике: Ковтун Людмила Григорьевна, а также олимпийский чемпион микросхемных наук Темников Сергей Николаевич).--></p>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<hr>
+
+	<div class="container">
+		<div class="row text-center" id="fh5co-features">
+			<div class="col-md-12 heading animate-box"><h2>Что же делать в такой ситуации...</h2></div>
+			<div class="col-md-4 col-sm-6 text-center fh5co-feature feature-box">
+				<div class="fh5co-feature-icon">
+				<!--
+					<i class="fa fa-eur fa-fw"></i>
+				-->
+				<img src="images/axe-128x128.png"  alt="">
+			</div>
+			<h3 class="heading"></h3>
+			<p></p>
+		</div>
+		<div class="col-md-4 col-sm-6 text-center fh5co-feature feature-box"> 
+			<div class="fh5co-feature-icon">
+			<!--
+				<i class="fa fa-dollar fa-fw"></i>
+			-->
+			<img  src="images/rasp-128x128.png" alt="">
+		</div>
+		<h3 class="heading"></h3>
+		<p></p>
+	</div>
+
+	<div class="clearfix visible-sm-block"></div>
+
+	<div class="col-md-4 col-sm-6 text-center fh5co-feature feature-box"> 
+		<div class="fh5co-feature-icon">
+			<!--<i class="fa fa-rub fa-fw"></i>-->
+			<img  src="images/measuring-tape-128x128.png" alt="">
+		</div>
+		<h3 class="heading"></h3>
+		<p></p>
+	</div>
+
+	<div class="clearfix visible-md-block visible-lg-block"></div>
+
+	<div class="col-md-4 col-sm-6 text-center fh5co-feature feature-box">
+		<div class="fh5co-feature-icon">
+			<!--<i class="fa fa-jpy fa-fw"></i>-->
+			<img  src="images/hammer-128x128.png" alt="">
+		</div>
+		<h3 class="heading"></h3>
+		<p></p>
+	</div>
+
+	<div class="clearfix visible-sm-block"></div>
+
+	<div class="col-md-4 col-sm-6 text-center fh5co-feature feature-box"> 
+		<div class="fh5co-feature-icon">
+			<!--<i class=""></i>-->
+		</div>
+		<h3 class="heading">И так..</h3>
+		<p>Это предметы, которые позволяют делать мне безумные вещи при любом раскладе. Просто приглядитесь ...</p>
+	</div>
+	<div class="col-md-4 col-sm-6 text-center fh5co-feature feature-box"> 
+
+		<div class="fh5co-feature-icon">
+			<!--<i class="fa fa-cc-visa fa-fw"></i>-->
+			<img  src="images/saw-2-128x128.png" alt="">
+		</div>
+		<h3 class="heading"></h3>
+		<p></p>
+	</div>
+</div>
+<hr>
+<!-- END row -->
+
+</div>
+<!-- END container -->
+
+<div class="animate-box fh5co-product-2">
+	<div class="fh5co-half img" style="background-image: url(images/img_8.jpg);">
+	</div>
+	<div class="fh5co-half">
+		<h3>Почему тащу?</h3>
+		<p>&mdash; На самом деле секрет успеха таится в мощном ПК и сильнейшей мотивации &ldquo; Всех порвать &ldquo;. С раннего детства родители выставили меня за порог дома со словами &ldquo; Сынок! Ты уже взрослый &ldquo;. Да мне было на тот момент 32 года , но все же.. Мне пришлось выживать в суровых условиях Благовещенска: я крал хлеб у голубей, крал у кошек пойманых ими голубей, крал жареных кошек у бомжей.. Да были времена. За место в жизни мне приходилось бороться! Я считаю, что именно двор развил во мне равнодушие, жестокость и умение постоять за себя. И именно эти качества помогают мне тащить! Я просто  не могу себе позволить проиграть!  </p>
+	</div>
+</div>
+<div id="fh5co-testimonial">
+
+	<div class="container">
+		<div class="row">
+			<!-- Start Slider Testimonial -->
+			
+			<h1 style="font-size: 22px;" class="fh5co-uppercase-heading-sm text-center animate-box">&mdash; Галерея &mdash;</h1>
+			<div style="text-align: center;" class="fh5co-spacer fh5co-spacer-xs">Да-да, это картинки заряженные тащерством, они безумно крутые.<br /> #НасыщаемсяНаслаждаемся...</div>
+	
+			<div class="owl-carousel-fullwidth animate-box">
+				<div class="item">
+					<img class="text-center quote" src="images/starwars-bb-8.png" style="width: 380px;" alt="">
+				</div>
+				<div class="item">
+					<img class="text-center quote" src="images/starwars-r2-d2.png" style="width: 380px;" alt="">
+				</div>
+				<div class="item">
+					<img class="text-center quote" src="images/starwars-stormtrooper.png" style="width: 380px;" alt="">
+				</div>
+				<div class="item">
+					<img class="text-center quote" src="images/starwars-boba-fett.png" style="width: 380px;" alt="">
+				</div>
+				<div class="item">
+					<img class="text-center quote" src="images/starwars-chewbacca.png" style="width: 380px;" alt="">
+				</div>
+				<div class="item">
+					<img class="text-center quote" src="images/starwars-darth-vader.png" style="width: 380px;" alt="">
+				</div>
+			</div>
+			<!-- End Slider Testimonial -->
+
+		</div>
+		<!-- END row -->
+	</div>
+</div>
+</div>
+<!-- END fhtco-main -->
+<footer role="contentinfo" id="fh5co-footer">
+	<a href="#" class="fh5co-arrow fh5co-gotop footer-box"><i class="ti-angle-up"></i></a>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-sm-12 footer-box">
+				<h3 class="fh5co-footer-heading">Связь со мной</h3>
+				<ul class="fh5co-social-icons">
+					<li><a href="https://vk.com/rpbln"><i class="fa fa-vk fa-fw"></i></a></li>
+					<li><a href="https://www.youtube.com/channel/UCbNkkW8O61bwScjYbdBFsDA"><i class="fa fa-youtube fa-fw"></i></a></li>
+				</ul>
+			</div>
+			<div class="col-md-4 col-sm-6 footer-box">
+				<h3 class="fh5co-footer-heading">Ресурсы</h3>
+				<ul class="fh5co-footer-links">
+					<!-- <li><a href="http://howdyho.net/">HowdyHo</a></li> -->
+					<li><a href="http://fontawesome.io/examples/">FontAwesome</a></li>
+					<!-- <li><a href="http://tympanus.net/codrops/category/tutorials/">Codrops</a></li> -->
+					<li><a href="https://unsplash.com/new">Unsplash</a></li>
+				</ul>
+			</div>
+
+			<div class="col-md-4 col-sm-6 footer-box">
+				<h3 class="fh5co-footer-heading">Карта сайта</h3>
+				<ul class="fh5co-footer-links">
+				<!--
+					<li><a href="php/signup.php">Sign up</a></li>
+					<li><a href="php/login.php">Log in</a></li>
+				-->
+			</ul>
+
+			<h1 id="fh5co-logo" class="pull-left"><a href="#"><img style="width: 70px;" src="/favicon.ico" alt=""></a></h1>
+		</div>
+		<div class="col-md-12 footer-box text-center">
+			<div class="fh5co-copyright">
+				<p>Copyright &copy; <?php echo date("Y");?>. All rights reserved.Ярослав Булава</p>
+				<p style="font-size: 12,4px;">Сайт создан исключительно в развлекательных целях и ни кагого вреда и ущерба не несёт!</p>
+			</div>
+		</div>
+	</div>
+	<!-- END row -->
+	<div class="fh5co-spacer fh5co-spacer-md"></div>
+</div>
+</footer>
+
+<script src="js/preloader.js" defer></script>
+<!-- jQuery Easing -->
+<script src="js/jquery.easing.1.3.js" defer></script>
+<!-- Bootstrap -->
+<script src="js/bootstrap.js" defer></script>
+<!-- Owl carousel -->
+<script src="js/owl.carousel.min.js" defer></script>
+<!-- Magnific Popup -->
+<script src="js/jquery.magnific-popup.min.js" defer></script>
+<!-- Superfish -->
+<script src="js/hoverIntent.js" defer></script>
+
+<script src="js/superfish.js" defer></script>
+<!-- Easy Responsive Tabs -->
+<script src="js/easyResponsiveTabs.js" defer></script>
+<!-- FastClick for Mobile/Tablets -->
+<script src="js/fastclick.js" defer></script>
+<!-- Parallax -->
+<script src="js/jquery.parallax-scroll.min.js" defer></script>
+<!-- Waypoints -->
+<script src="js/jquery.waypoints.min.js" defer></script>
+<!-- Main JS -->
+<script src="js/main.js" defer></script>
+
+<script src="js/form_1.js" defer></script>
+
+</body>
+</html>
